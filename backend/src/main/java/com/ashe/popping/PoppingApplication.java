cd9e59807e49192c8d3e692381630eecf82ef65c
jxixeun <jxixeun@gmail.com>
2023-07-13 10:45:52 +0900

    :tada: init: 프로젝트 생성

---
+++ backend/src/main/java/com/ashe/popping/PoppingApplication.java
@@ -0,0 +1,13 @@
+package com.ashe.popping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoppingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ashe/popping/PoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashe.popping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
