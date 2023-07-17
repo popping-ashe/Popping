ddb7c661285ba6dcf5b42bb550d724506212a4da
fluffymn <mineezm9@gmail.com>
2023-07-17 16:46:38 +0900

    ✨ feat: Member Service에 `createMember` 추상 메서드 생성

--- backend/src/main/java/com/ashe/popping/domain/member/service/MemberService.java
+++ backend/src/main/java/com/ashe/popping/domain/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.ashe.popping.domain.member.service;
 import com.ashe.popping.api.member.dto.MemberDto;
 
 public interface MemberService {
+	MemberDto createMember(MemberDto memberDto);
 	MemberDto getMemberByMemberId(Long memberId);
 	MemberDto updateMember(MemberDto memberDto);
 }
