3e582fd2ef3a8e6a6c98f7d0b0525c6734e4f2b7
양정훈 <yjhoon3658@naver.com>
2023-07-17 17:44:31 +0900

    :sparkles: feat: MessageRepository에 findByReceiver 작성

--- backend/src/main/java/com/ashe/popping/domain/message/repository/MessageRepository.java
+++ backend/src/main/java/com/ashe/popping/domain/message/repository/MessageRepository.java
@@ -1,8 +1,11 @@
 package com.ashe.popping.domain.message.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ashe.popping.domain.message.entity.Message;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+	public List<Message> findByReceiver(Long receiver);
 }
