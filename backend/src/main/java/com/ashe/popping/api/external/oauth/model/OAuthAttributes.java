e2e2817590e6242e4c82c89b5d7efb683ef110d0
jungeunevepark <evecomcom98@naver.com>
2023-07-17 17:41:47 +0900

    :sparkles: feat: OAuthAttributes 생성

---
+++ backend/src/main/java/com/ashe/popping/api/external/oauth/model/OAuthAttributes.java
@@ -0,0 +1,10 @@
+package com.ashe.popping.api.external.oauth.model;
+
+import com.ashe.popping.domain.member.constant.Role;
+import com.ashe.popping.domain.member.entity.Member;
+
+public class OAuthAttributes {
+	private String uuid;
+	private String nickname;
+
+}
