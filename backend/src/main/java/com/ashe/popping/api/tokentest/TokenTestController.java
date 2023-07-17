954e78014550b26c3664f5f8e0552e41bf3f6727
jungeunevepark <evecomcom98@naver.com>
2023-07-17 17:35:11 +0900

    :sparkles: feat: 토큰 유효성 검사 추가

--- backend/src/main/java/com/ashe/popping/api/tokentest/TokenTestController.java
+++ backend/src/main/java/com/ashe/popping/api/tokentest/TokenTestController.java
@@ -2,12 +2,14 @@ package com.ashe.popping.api.tokentest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ashe.popping.domain.member.constant.Role;
 import com.ashe.popping.global.jwt.dto.JwtTokenDto;
 import com.ashe.popping.global.jwt.service.TokenManager;
 
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,5 +24,16 @@ public class TokenTestController {
 	@GetMapping("/create")
 	public JwtTokenDto createJwtTokenDto(){
 		return tokenManager.createJwtTokenDto(1L, Role.ADMIN);
+	}
+
+	@GetMapping("/valid")
+	public String validateJwtToken(@RequestParam String token){
+		tokenManager.validateToken(token);
+		Claims tokenClaims = tokenManager.getTokenClaims(token);
+		long memberId = Long.valueOf((Integer) tokenClaims.get("memberId"));
+		String role = (String) tokenClaims.get("role");
+		log.info("memberId : {}", memberId);
+		log.info("role : {}", role);
+		return "success";
 	}
 }
