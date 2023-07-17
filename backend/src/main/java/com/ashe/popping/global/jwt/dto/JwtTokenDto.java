38d028784ff12622f6028cda84c5d27d089e4bb5
jungeunevepark <evecomcom98@naver.com>
2023-07-17 17:12:16 +0900

    :pencil2: fix: token 만료 시간을 한국 시간대로 수정

--- backend/src/main/java/com/ashe/popping/global/jwt/dto/JwtTokenDto.java
+++ backend/src/main/java/com/ashe/popping/global/jwt/dto/JwtTokenDto.java
@@ -2,6 +2,8 @@ package com.ashe.popping.global.jwt.dto;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.*;
 
 @Getter
@@ -13,7 +15,9 @@ public class JwtTokenDto {
 
 	private String grantType;
 	private String accessToken;
+	@JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
 	private Date accessTokenExpireTime;
 	private String refreshToken;
+	@JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
 	private Date refreshTokenExpireTime;
 }
