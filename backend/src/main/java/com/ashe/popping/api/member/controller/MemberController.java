263e51d759aa4276478624a9e308a38514467ecf
fluffymn <mineezm9@gmail.com>
2023-07-17 16:37:47 +0900

    ✨ feat: 멤버의 닉네임을 변경하는 API PATCH `/members/{memberId}` 구현

--- backend/src/main/java/com/ashe/popping/api/member/controller/MemberController.java
+++ backend/src/main/java/com/ashe/popping/api/member/controller/MemberController.java
@@ -1,11 +1,14 @@
 package com.ashe.popping.api.member.controller;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,4 +32,19 @@ public class MemberController {
 
 		return ResponseEntity.status(HttpStatus.OK).body(result);
 	}
+
+	@PatchMapping("/{memberId}")
+	public ResponseEntity<MemberDto.Response> modifyMember(@PathVariable("memberId") Long memberId, @RequestBody MemberDto.Request member) {
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		MemberDto memberDto = mapper.map(member, MemberDto.class);
+		memberDto.setMemberId(memberId);
+
+		memberService.updateMember(memberDto);
+
+		MemberDto.Response result = new ModelMapper().map(memberDto, MemberDto.Response.class);
+
+		return ResponseEntity.status(HttpStatus.OK).body(result);
+	}
+
 }
