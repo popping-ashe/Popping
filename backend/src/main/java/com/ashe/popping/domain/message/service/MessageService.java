446df7730e3574888ac8560175866300613483bf
jxixeun <jxixeun@gmail.com>
2023-07-18 10:54:56 +0900

    :sparkles: feat: MessageService 인터페이스 생성

--- backend/src/main/java/com/ashe/popping/domain/message/service/MessageService.java
+++ backend/src/main/java/com/ashe/popping/domain/message/service/MessageService.java
@@ -2,32 +2,11 @@ package com.ashe.popping.domain.message.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.ashe.popping.domain.message.dto.MessageDto;
-import com.ashe.popping.domain.message.entity.Message;
-import com.ashe.popping.domain.message.repository.MessageRepository;
 
-import lombok.RequiredArgsConstructor;
+public interface MessageService {
 
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class MessageService {
-
-	private final MessageRepository messageRepository;
-
-	public Message saveMessage(MessageDto messageDto) {
-		Message message = Message.from(messageDto);
-		return messageRepository.save(message);
-	}
-
-	public List<Message> loadReceiveMessage(Long receiver) {
-		return messageRepository.findByReceiver(receiver);
-	}
-
-	public List<Message> loadSenderMessage(Long sender) {
-		return messageRepository.findBySender(sender);
-	}
+	MessageDto saveMessage(MessageDto messageDto);
+	List<MessageDto> loadReceiveMessage(Long receiver);
+	List<MessageDto> loadSenderMessage(Long sender);
 }
