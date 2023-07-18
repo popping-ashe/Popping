b759e98907065dbd7ab40046fe64098d060bba51
jungeunevepark <evecomcom98@naver.com>
2023-07-18 10:51:59 +0900

    :sparkles: feat: Feign Client 관련 Configuration 추가

---
+++ backend/src/main/java/com/ashe/popping/global/config/CoreFeignConfig.java
@@ -0,0 +1,30 @@
+package com.ashe.popping.global.config;
+
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import feign.Logger;
+import feign.codec.Encoder;
+import feign.form.FormEncoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.ashe")
+public class CoreFeignConfig {
+	@Bean
+	Encoder formEncoder(ObjectFactory<HttpMessageConverters> converters){
+		return new SpringFormEncoder(new SpringEncoder(converters));
+		// return new feign.form.FormEncoder();
+	}
+
+	@Bean
+	Logger.Level feignLoggerLevel(){
+		return Logger.Level.FULL;
+	}
+
+}
