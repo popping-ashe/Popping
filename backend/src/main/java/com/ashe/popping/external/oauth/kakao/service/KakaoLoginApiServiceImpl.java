431cee733fd4492b3947702bf85a5d7ec557c823
jungeunevepark <evecomcom98@naver.com>
2023-07-18 11:21:24 +0900

    :sparkles: feat: KakaoLoginApiServiceImpl 생성

---
+++ backend/src/main/java/com/ashe/popping/external/oauth/kakao/service/KakaoLoginApiServiceImpl.java
@@ -0,0 +1,34 @@
+package com.ashe.popping.external.oauth.kakao.service;
+
+import org.springframework.stereotype.Service;
+
+import com.ashe.popping.external.oauth.kakao.client.KakaoMemberInfoClient;
+import com.ashe.popping.external.oauth.kakao.dto.KakaoMemberInfoResponseDto;
+import com.ashe.popping.external.oauth.model.OAuthAttributes;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class KakaoLoginApiServiceImpl implements KakaoLoginApiService{
+	private final KakaoMemberInfoClient kakaoMemberInfoClient;
+	private final String contentType = "application/x-www-form-urlencoded;charset=utf-8";
+
+	public OAuthAttributes getMemberInfo(String settingAccessToken){
+
+		KakaoMemberInfoResponseDto kakaoMemberInfoResponseDto = kakaoMemberInfoClient.getKakaoMemberInfo(contentType, settingAccessToken);
+		System.out.println("kakaoLoginApiServiceImpl: "+kakaoMemberInfoResponseDto);
+		KakaoMemberInfoResponseDto.KakaoAccount kakaoAccount = kakaoMemberInfoResponseDto.getKakaoAccount();
+
+		return OAuthAttributes.builder()
+			.kakaoId(kakaoMemberInfoResponseDto.getKakaoId())
+			.nickname(kakaoAccount.getProfile().getNickname())
+			.build();
+	}
+
+}
