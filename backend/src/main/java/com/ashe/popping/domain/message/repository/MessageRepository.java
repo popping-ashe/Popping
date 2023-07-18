21c97049dc5580a6f95ec3ee483d04e94eaceab8
jxixeun <jxixeun@gmail.com>
2023-07-18 10:58:05 +0900

    :pencil2: fix: public 접근 제어자 삭제

--- backend/src/main/java/com/ashe/popping/domain/message/repository/MessageRepository.java
+++ backend/src/main/java/com/ashe/popping/domain/message/repository/MessageRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ashe.popping.domain.message.entity.Message;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-	public List<Message> findByReceiver(Long receiver);
+	List<Message> findByReceiver(Long receiver);
 
-	public List<Message> findBySender(Long sender);
+	List<Message> findBySender(Long sender);
 }
