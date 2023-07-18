6f76d1d141f2e17c3011623d7ac897289448c861
jungeunevepark <evecomcom98@naver.com>
2023-07-18 11:20:59 +0900

    :sparkles: feat: OAuthAttributes에 멤버 변수 반환 함수 추가

--- backend/src/main/java/com/ashe/popping/external/oauth/model/OAuthAttributes.java
+++ backend/src/main/java/com/ashe/popping/external/oauth/model/OAuthAttributes.java
@@ -1,17 +1,26 @@
 package com.ashe.popping.external.oauth.model;
 
+import com.ashe.popping.domain.member.constant.Role;
+import com.ashe.popping.domain.member.entity.Member;
+
+import lombok.Builder;
+import lombok.Getter;
 import lombok.ToString;
 
+@Getter
+@Builder
 @ToString
 public class OAuthAttributes {
 	private String kakaoId;
 	private String nickname;
 	
-	
-	// 멤버 엔티티 빌드 패턴 추가 후 함수 생성
-	public OAuthAttributes of(String kakaoId, String nickname){
-		this.kakaoId = kakaoId; this.nickname = nickname;
-		return null;
+
+	public Member toMemberEntity(Role role){
+		return Member.builder()
+			.kakaoId(kakaoId)
+			.nickname(nickname)
+			.role(role)
+			.build();
 	}
 
 }
