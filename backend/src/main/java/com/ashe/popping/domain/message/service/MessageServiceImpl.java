de02321378cb5a9dee4a8b227ec054e42b434081
jxixeun <jxixeun@gmail.com>
2023-07-18 10:58:43 +0900

    :sparkles: feat: MessageService 구현 및 리턴 타입 MessageDto로 변경

---
+++ backend/src/main/java/com/ashe/popping/domain/message/service/MessageServiceImpl.java
@@ -0,0 +1,40 @@
+package com.ashe.popping.domain.message.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ashe.popping.domain.message.dto.MessageDto;
+import com.ashe.popping.domain.message.entity.Message;
+import com.ashe.popping.domain.message.repository.MessageRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MessageServiceImpl implements MessageService{
+
+	private final MessageRepository messageRepository;
+
+	public MessageDto saveMessage(MessageDto messageDto) {
+		Message message = Message.from(messageDto);
+		messageRepository.save(message);
+		return MessageDto.from(message);
+	}
+
+	public List<MessageDto> loadReceiveMessage(Long receiver) {
+		List<Message> messages = messageRepository.findByReceiver(receiver);
+		return messages.stream()
+			.map(MessageDto::from)
+			.toList();
+	}
+
+	public List<MessageDto> loadSenderMessage(Long sender) {
+		List<Message> messages = messageRepository.findBySender(sender);
+		return messages.stream()
+			.map(MessageDto::from)
+			.toList();
+	}
+}
