4e9452beb89110c7860c8983e557749cd332b77b
jungeunevepark <evecomcom98@naver.com>
2023-07-18 15:50:39 +0900

    :sparkles: feat: 카카오 로그인 시, 회원가입과 로그인 분류 기능 구현

--- backend/src/main/java/com/ashe/popping/external/oauth/kakao/service/KakaoLoginApiServiceImpl.java
+++ backend/src/main/java/com/ashe/popping/external/oauth/kakao/service/KakaoLoginApiServiceImpl.java
@@ -1,10 +1,18 @@
 package com.ashe.popping.external.oauth.kakao.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.ashe.popping.api.member.dto.MemberDto;
+import com.ashe.popping.domain.member.constant.Role;
+import com.ashe.popping.domain.member.entity.Member;
+import com.ashe.popping.domain.member.service.MemberService;
 import com.ashe.popping.external.oauth.kakao.client.KakaoMemberInfoClient;
 import com.ashe.popping.external.oauth.kakao.dto.KakaoMemberInfoResponseDto;
 import com.ashe.popping.external.oauth.model.OAuthAttributes;
+import com.ashe.popping.global.jwt.dto.JwtTokenDto;
+import com.ashe.popping.global.jwt.service.TokenManager;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -17,14 +25,32 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional
 public class KakaoLoginApiServiceImpl implements KakaoLoginApiService{
 	private final KakaoMemberInfoClient kakaoMemberInfoClient;
+	private final TokenManager tokenManager;
+	private final MemberService memberService;
 	private final String contentType = "application/x-www-form-urlencoded;charset=utf-8";
 
 	public OAuthAttributes getMemberInfo(String settingAccessToken){
 
 		KakaoMemberInfoResponseDto kakaoMemberInfoResponseDto = kakaoMemberInfoClient.getKakaoMemberInfo(contentType, settingAccessToken);
-		System.out.println("kakaoLoginApiServiceImpl: "+kakaoMemberInfoResponseDto);
 		KakaoMemberInfoResponseDto.KakaoAccount kakaoAccount = kakaoMemberInfoResponseDto.getKakaoAccount();
+		OAuthAttributes memberInfo = OAuthAttributes.of(kakaoMemberInfoResponseDto.getKakaoId(), kakaoAccount.getProfile().getNickname());
+		log.info("memberInfo : {}", memberInfo);
+		JwtTokenDto jwtTokenDto;
+		Optional<Member> optionalMember = memberService.getMemberByKakaoId(memberInfo.getKakaoId());
 
+		// 1. 신규 회원
+		if(optionalMember.isEmpty()){
+			MemberDto oauthMember = memberInfo.toMemberDto(Role.USER);
+			oauthMember = memberService.createMember(oauthMember);
+			// 토큰 생성
+			jwtTokenDto = tokenManager.createJwtTokenDto(oauthMember.getMemberId(), oauthMember.getRole());
+		}
+		// 2. 가입된 회원
+		else{
+			Member oauthMember = optionalMember.get();
+			// 토큰 생성
+			jwtTokenDto = tokenManager.createJwtTokenDto(oauthMember.getMemberId(), oauthMember.getRole());
+		}
 		return OAuthAttributes.of(kakaoMemberInfoResponseDto.getKakaoId(), kakaoAccount.getProfile().getNickname());
 	}
 
