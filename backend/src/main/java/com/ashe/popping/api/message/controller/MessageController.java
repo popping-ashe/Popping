2c6c83aa153e4314694a74b58b75ffc5b0e6a200
양정훈 <yjhoon3658@naver.com>
2023-07-18 17:13:41 +0900

    :art: refactor: 생성자를 이용한 의존성 주입 @RequiredArgsConstructor 애노테이션으로 대체

--- backend/src/main/java/com/ashe/popping/api/message/controller/MessageController.java
+++ backend/src/main/java/com/ashe/popping/api/message/controller/MessageController.java
@@ -1,6 +1,5 @@
 package com.ashe.popping.api.message.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,21 +10,18 @@ import com.ashe.popping.api.message.dto.MessageApiDto;
 import com.ashe.popping.domain.message.dto.MessageDto;
 import com.ashe.popping.domain.message.service.MessageService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/messages")
+@RequiredArgsConstructor
 public class MessageController {
 	// 요청으로 몇시간 후 만료인지 시간이 들어옴...
 	private final MessageService messageService;
-
-	@Autowired
-	public MessageController(MessageService messageService) {
-		this.messageService = messageService;
-	}
 
 	@PostMapping
 	public ResponseEntity<MessageApiDto.Response> sendMessage(@RequestBody MessageApiDto.Request request) {
 		MessageDto messageDto = messageService.saveMessage(MessageDto.from(request));
 		return ResponseEntity.ok(MessageApiDto.Response.from(messageDto));
 	}
-
 }
