9994fe120601f89301167108258a20451e8e37d8
fluffymn <mineezm9@gmail.com>
2023-07-18 14:08:50 +0900

    ✏️ fix: `MemberRepository`에서 `JpaRepository` 상속하도록 수정

--- backend/src/main/java/com/ashe/popping/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/ashe/popping/domain/member/repository/MemberRepository.java
@@ -1,9 +1,9 @@
 package com.ashe.popping.domain.member.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ashe.popping.domain.member.entity.Member;
 
-public interface MemberRepository extends CrudRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 	Member findByMemberId(Long memberId);
 }
