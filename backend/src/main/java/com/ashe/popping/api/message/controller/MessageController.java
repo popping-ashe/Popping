fa7e05fae127fa6b7fcd695790a561c0d544a7eb
jxixeun <jxixeun@gmail.com>
2023-07-18 16:45:03 +0900

    :sparkles: feat: 메세지 전송 API 구현

--- backend/src/main/java/com/ashe/popping/api/message/controller/MessageController.java
+++ backend/src/main/java/com/ashe/popping/api/message/controller/MessageController.java
@@ -1,9 +1,14 @@
 package com.ashe.popping.api.message.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ashe.popping.api.message.dto.MessageApiDto;
+import com.ashe.popping.domain.message.dto.MessageDto;
 import com.ashe.popping.domain.message.service.MessageService;
 
 @RestController
@@ -15,6 +20,12 @@ public class MessageController {
 	@Autowired
 	public MessageController(MessageService messageService) {
 		this.messageService = messageService;
+	}
+
+	@PostMapping
+	public ResponseEntity<MessageApiDto.Response> sendMessage(@RequestBody MessageApiDto.Request request) {
+		MessageDto messageDto = messageService.saveMessage(MessageDto.from(request));
+		return ResponseEntity.ok(MessageApiDto.Response.from(messageDto));
 	}
 
 }
