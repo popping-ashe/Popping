53a043c8d78686e1a9d1234a9e45e65b96cd3ea2
fluffymn <mineezm9@gmail.com>
2023-07-18 14:34:52 +0900

    ✨ feat: kakaoId로 멤버 데이터 가져오는 쿼리 메서드 `findByKakaoId` 생성

--- backend/src/main/java/com/ashe/popping/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/ashe/popping/domain/member/repository/MemberRepository.java
@@ -1,9 +1,13 @@
 package com.ashe.popping.domain.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ashe.popping.domain.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	Member findByMemberId(Long memberId);
+
+	Optional<Member> findByKakaoId(String kakaoId);
 }
