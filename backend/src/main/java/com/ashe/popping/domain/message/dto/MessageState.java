62b69595549f27cfacccc3dec5376eb51b1d3ce3
양정훈 <yjhoon3658@naver.com>
2023-07-19 09:09:12 +0900

    :sparkles: feat: MessageState Enum 에 Message 필드 추가

--- backend/src/main/java/com/ashe/popping/domain/message/dto/MessageState.java
+++ backend/src/main/java/com/ashe/popping/domain/message/dto/MessageState.java
@@ -4,13 +4,15 @@ import lombok.Getter;
 
 @Getter
 public enum MessageState {
-	EXPIRED(-1),
-	UNREAD(0),
-	READ(1);
+	EXPIRED(-1, "만료됨"),
+	UNREAD(0, "안읽음"),
+	READ(1, "읽음");
 
-	MessageState(int code) {
+	MessageState(int code, String message) {
 		this.code = code;
+		this.message = message;
 	}
 
 	private final int code;
+	private final String message;
 }
