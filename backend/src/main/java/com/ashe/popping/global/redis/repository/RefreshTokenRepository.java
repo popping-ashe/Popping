ff0ae2ffffbc47cf94227742864d6cb0827e5ae3
jungeunevepark <evecomcom98@naver.com>
2023-07-19 16:16:29 +0900

    :bug: fix: ValueOperations를 <String, String>으로 변경하여 cast exception 수정

--- backend/src/main/java/com/ashe/popping/global/redis/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/ashe/popping/global/redis/repository/RefreshTokenRepository.java
@@ -1,10 +1,6 @@
 package com.ashe.popping.global.redis.repository;
 
-import java.util.Objects;
-import java.util.Optional;
 import java.util.concurrent.TimeUnit;
-
-import javax.swing.text.html.Option;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -19,19 +15,10 @@ import lombok.RequiredArgsConstructor;
 public class RefreshTokenRepository {
 	private final RedisTemplate redisTemplate;
 
-	public void save(final RefreshToken refreshToken){
-		ValueOperations<String, Long> valueOperations = redisTemplate.opsForValue();
-		valueOperations.set(refreshToken.getRefreshToken(), refreshToken.getMemberId());
+	public void save(RefreshToken refreshToken) {
+		ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+		valueOperations.set(String.valueOf(refreshToken.getMemberId()), refreshToken.getRefreshToken());
 		redisTemplate.expire(refreshToken.getRefreshToken(), 14, TimeUnit.DAYS); // 14일후에 만료
 	}
 
-	public Optional<RefreshToken> findByMemberId(String refreshToken){
-		ValueOperations<String, Long> valueOperations = redisTemplate.opsForValue();
-		Long memberId = valueOperations.get(refreshToken);
-
-		if(Objects.isNull(memberId)){
-			return Optional.empty();
-		}
-		return Optional.of(new RefreshToken(refreshToken, memberId));
-	}
 }
