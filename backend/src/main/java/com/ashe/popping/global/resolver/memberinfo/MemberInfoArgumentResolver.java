0bc254715b66174c4476054582d1e00383d769e3
jungeunevepark <evecomcom98@naver.com>
2023-07-19 11:09:03 +0900

    :sparkles: feat: MemberInfoArumentResolver에 resolveArgument 함수 구현

--- backend/src/main/java/com/ashe/popping/global/resolver/memberinfo/MemberInfoArgumentResolver.java
+++ backend/src/main/java/com/ashe/popping/global/resolver/memberinfo/MemberInfoArgumentResolver.java
@@ -30,5 +30,21 @@ public class MemberInfoArgumentResolver implements HandlerMethodArgumentResolver
 		boolean hasMemberInfoDto = MemberInfoDto.class.isAssignableFrom(parameter.getParameterType());
 		return hasMemberInfoAnnotaion && hasMemberInfoDto;
 	}
-
+	/*
+	* 이 메소드의 반환값이 대상이 되는 메소드의 파라미터에 바인딩된다.
+	*
+	* @MemberInfo 어노테이션이 호출되었을 때 실행되는 부분
+	* */
+	@Override
+	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+		// 토큰에 있던 정보를 MemberInfoDto로 반들어 반환
+		HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+		String authorizationHeader = request.getHeader("Authorization");
+		String token = authorizationHeader.split(" ")[1];
+		Claims tokenClaims = tokenManager.getTokenClaims(token);
+		Long memberId = Long.valueOf((Integer) tokenClaims.get("memberId"));
+		String role = (String) tokenClaims.get("role");
+		return MemberInfoDto.of(memberId, role);
+	}
 }
