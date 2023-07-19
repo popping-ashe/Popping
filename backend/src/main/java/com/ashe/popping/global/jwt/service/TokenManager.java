79fe7c64623826fab52493f8d536c3386ce19881
jungeunevepark <evecomcom98@naver.com>
2023-07-19 16:20:50 +0900

    :art: refactor: tokenManager CRLF를 LF로 수정

--- backend/src/main/java/com/ashe/popping/global/jwt/service/TokenManager.java
+++ backend/src/main/java/com/ashe/popping/global/jwt/service/TokenManager.java
@@ -4,7 +4,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 import com.ashe.popping.domain.member.constant.Role;
-import com.ashe.popping.domain.member.repository.MemberRepository;
 import com.ashe.popping.global.error.ErrorCode;
 import com.ashe.popping.global.error.exception.AuthenticationException;
 import com.ashe.popping.global.jwt.contant.GrantType;
@@ -20,12 +19,11 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-
 @Slf4j
 @RequiredArgsConstructor
 public class TokenManager {
 
-	private final MemberRepository memberRepository;
+	// private final MemberRepository memberRepository;
 	private final RefreshTokenRepository refreshTokenRepository;
 
 	// application.yml에 설정한 값을 가져온다.
@@ -33,61 +31,61 @@ public class TokenManager {
 	private final String refreshTokenExpirationTime;
 	private final String tokenSecret;
 
-
-	public Date createAccessTokenExpireTime(){
-		return new Date(System.currentTimeMillis()+Long.parseLong(accessTokenExpirationTime));
+	public Date createAccessTokenExpireTime() {
+		return new Date(System.currentTimeMillis() + Long.parseLong(accessTokenExpirationTime));
 	}
 
-	public Date createRefreshTokenExpireTime(){
-		return new Date(System.currentTimeMillis()+Long.parseLong(refreshTokenExpirationTime));
+	public Date createRefreshTokenExpireTime() {
+		return new Date(System.currentTimeMillis() + Long.parseLong(refreshTokenExpirationTime));
 	}
 
-	public String createAccessToken(Long memberId, Role role, Date expirationTime){
+	public String createAccessToken(Long memberId, Role role, Date expirationTime) {
 		return createToken(TokenType.ACCESS.name(), memberId, role, expirationTime);
 	}
 
-	public String createRefreshToken(Long memberId, Role role, Date expirationTime){
+	public String createRefreshToken(Long memberId, Role role, Date expirationTime) {
 		String refreshToken = createToken(TokenType.REFRESH.name(), memberId, role, expirationTime);
-		RefreshToken redisRefreshToken = new RefreshToken(refreshToken, memberId);
+		RefreshToken redisRefreshToken = new RefreshToken(memberId, refreshToken);
 		refreshTokenRepository.save(redisRefreshToken);
 		return refreshToken;
 	}
 
-	public JwtTokenDto createJwtTokenDto(Long memberId, Role role){
+	public JwtTokenDto createJwtTokenDto(Long memberId, Role role) {
 		Date accessTokenExpirationTime = createAccessTokenExpireTime();
 		Date refreshTokenExpirationTime = createRefreshTokenExpireTime();
 		String accessToken = createAccessToken(memberId, role, accessTokenExpirationTime);
 		String refreshToken = createRefreshToken(memberId, role, refreshTokenExpirationTime);
-		return JwtTokenDto.of(GrantType.BEARER.getType(), accessToken, accessTokenExpirationTime, refreshToken, refreshTokenExpirationTime);
+		return JwtTokenDto.of(GrantType.BEARER.getType(), accessToken, accessTokenExpirationTime, refreshToken,
+			refreshTokenExpirationTime);
 	}
 
-	public void validateToken(String token){
-		try{
+	public void validateToken(String token) {
+		try {
 			Jwts.parser().setSigningKey(tokenSecret.getBytes(StandardCharsets.UTF_8))
 				.parseClaimsJws(token).getBody();
-		}catch(ExpiredJwtException e){
+		} catch (ExpiredJwtException e) {
 			log.info("token 만료", e);
 			throw new com.ashe.popping.global.error.exception.AuthenticationException(ErrorCode.TOKEN_EXPIRED);
-		}catch(Exception e){
+		} catch (Exception e) {
 			log.info("유효하지 않은 token", e);
 			throw new AuthenticationException(ErrorCode.NOT_VALID_TOKEN);
 		}
 
 	}
 
-	public Claims getTokenClaims(String token){
+	public Claims getTokenClaims(String token) {
 		Claims claims;
-		try{
+		try {
 			claims = Jwts.parser().setSigningKey(tokenSecret.getBytes(StandardCharsets.UTF_8))
 				.parseClaimsJws(token).getBody();
-		} catch(Exception e){
+		} catch (Exception e) {
 			log.info("유효하지 않은 token", e);
 			throw new AuthenticationException(ErrorCode.NOT_VALID_TOKEN);
 		}
 		return claims;
 	}
 
-	public String createToken(String tokenType, Long memberId, Role role, Date expirationTime){
+	public String createToken(String tokenType, Long memberId, Role role, Date expirationTime) {
 		return Jwts.builder()
 			.setSubject(tokenType)
 			.setIssuedAt(new Date())
@@ -99,4 +97,4 @@ public class TokenManager {
 			.compact();
 	}
 
- }
+}
