3d3a101617c7d3a998d2f6c057cd1bca903f5c18
jungeunevepark <evecomcom98@naver.com>
2023-07-19 12:57:03 +0900

    :sparkles: feat: Redis에서 refreshToken을 가진 memberId를 반환하는 함수 생성

--- backend/src/main/java/com/ashe/popping/global/redis/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/ashe/popping/global/redis/repository/RefreshTokenRepository.java
@@ -1,6 +1,10 @@
 package com.ashe.popping.global.redis.repository;
 
+import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+
+import javax.swing.text.html.Option;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -19,5 +23,15 @@ public class RefreshTokenRepository {
 		ValueOperations<String, Long> valueOperations = redisTemplate.opsForValue();
 		valueOperations.set(refreshToken.getRefreshToken(), refreshToken.getMemberId());
 		redisTemplate.expire(refreshToken.getRefreshToken(), 14, TimeUnit.DAYS); // 14일후에 만료
+	}
+
+	public Optional<RefreshToken> findByMemberId(String refreshToken){
+		ValueOperations<String, Long> valueOperations = redisTemplate.opsForValue();
+		Long memberId = valueOperations.get(refreshToken);
+
+		if(Objects.isNull(memberId)){
+			return Optional.empty();
+		}
+		return Optional.of(new RefreshToken(refreshToken, memberId));
 	}
 }
