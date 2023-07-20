4c1a8bac4a91d21ff80fa9f5f282c98eb3f43616
jungeunevepark <evecomcom98@naver.com>
2023-07-20 10:33:26 +0900

    :sparkles: feat: token 재발급 api 구현

---
+++ backend/src/main/java/com/ashe/popping/api/token/controller/TokenController.java
@@ -0,0 +1,30 @@
+package com.ashe.popping.api.token.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ashe.popping.api.token.service.TokenService;
+import com.ashe.popping.global.jwt.dto.JwtTokenDto;
+import com.ashe.popping.global.util.AuthorizationHeaderUtils;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class TokenController {
+	private final TokenService tokenService;
+
+	@PostMapping("/access-token/issue")
+	public ResponseEntity<JwtTokenDto> createToken(HttpServletRequest httpServletRequest) {
+		String authorizationHeader = httpServletRequest.getHeader("Authorization");
+		AuthorizationHeaderUtils.validateAuthorization(authorizationHeader);
+		String refreshToken = authorizationHeader.split(" ")[1];
+		JwtTokenDto jwtTokenDto = tokenService.createTokenByRefreshToken(refreshToken);
+		return ResponseEntity.ok(jwtTokenDto);
+
+	}
+}
