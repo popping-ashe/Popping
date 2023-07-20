aee8af315c7779750f31e001ffccf68479d3de68
jungeunevepark <evecomcom98@naver.com>
2023-07-20 10:31:42 +0900

    :sparkles: feat: TokenType이 Access인지 확인하는 함수 구현

--- backend/src/main/java/com/ashe/popping/global/jwt/contant/TokenType.java
+++ backend/src/main/java/com/ashe/popping/global/jwt/contant/TokenType.java
@@ -2,4 +2,8 @@ package com.ashe.popping.global.jwt.contant;
 
 public enum TokenType {
 	ACCESS, REFRESH;
+
+	public static boolean isAccessToken(String tokenType) {
+		return TokenType.ACCESS.name().equals(tokenType);
+	}
 }
