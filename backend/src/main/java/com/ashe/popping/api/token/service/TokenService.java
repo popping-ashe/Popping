6faa2550e05f99357546f200bd85b6c924d8afaa
jungeunevepark <evecomcom98@naver.com>
2023-07-20 10:32:56 +0900

    :sparkles: feat: token이 유효하지 않은 경우 토큰 만료처리, 반환 값을 jwt token으로 변경

--- backend/src/main/java/com/ashe/popping/api/token/service/TokenService.java
+++ backend/src/main/java/com/ashe/popping/api/token/service/TokenService.java
@@ -5,11 +5,11 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
-import com.ashe.popping.api.token.dto.AccessTokenResponseDto;
 import com.ashe.popping.domain.member.dto.MemberDto;
 import com.ashe.popping.domain.member.service.MemberService;
 import com.ashe.popping.global.error.ErrorCode;
 import com.ashe.popping.global.error.exception.AuthenticationException;
+import com.ashe.popping.global.jwt.dto.JwtTokenDto;
 import com.ashe.popping.global.jwt.service.TokenManager;
 import com.ashe.popping.global.redis.dto.RefreshToken;
 import com.ashe.popping.global.redis.repository.RefreshTokenRepository;
@@ -26,7 +26,7 @@ public class TokenService {
 	private final TokenManager tokenManager;
 	private final RefreshTokenRepository refreshTokenRepository;
 
-	public AccessTokenResponseDto createAccessTokenByRefreshToken(String refreshToken) {
+	public JwtTokenDto createTokenByRefreshToken(String refreshToken) {
 		// 1. refreshToken으로 memberId 반환 받기
 		Long memberId = 0L;
 		try {
@@ -34,22 +34,23 @@ public class TokenService {
 			memberId = Long.valueOf((Integer)tokenClaims.get("memberId"));
 		} catch (Exception e) {
 			// 로그아웃 처리
+			// refreshToken 만료 처리하기!!
+			tokenManager.expireRefreshToken(memberId);
 			throw new AuthenticationException(ErrorCode.REFRESH_TOKEN_EXPIRED); // 만료된 토큰
 		}
 		// 2. memberId로 refreshToken 레디스에서 가져오기
 		Optional<RefreshToken> refreshTokenDto = refreshTokenRepository.findRefreshTokenByMemberId(memberId);
 		// 3. client로부터 받은 refreshToken과 Redis에 저장된 refreshToken을 비교하기.
 		// 받은 refreshToken과 비교
-		if (refreshTokenDto.get().getRefreshToken().equals(refreshToken)) {
+		if (!(refreshTokenDto.isEmpty()) && refreshTokenDto.get().getRefreshToken().equals(refreshToken)) {
 			MemberDto memberDto = memberService.getMemberByMemberId(memberId);
-			System.out.println(memberDto);
 			Date accessTokenExpireTime = tokenManager.createAccessTokenExpireTime();
 			String accessToken = tokenManager.createAccessToken(memberId, memberDto.getRole(),
 				accessTokenExpireTime);
-			return AccessTokenResponseDto.of(accessToken, accessTokenExpireTime);
+			return tokenManager.createJwtTokenDto(memberDto.getMemberId(), memberDto.getRole());
 		} else {
 			// 다른 경우
-			// 악의적 이용 -> 로그아웃
+			tokenManager.expireRefreshToken(memberId);
 			throw new AuthenticationException(ErrorCode.NOT_VALID_TOKEN);
 		}
 
