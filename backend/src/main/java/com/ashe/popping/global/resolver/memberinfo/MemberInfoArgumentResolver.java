1227478ca3f1cd1c7695056a2f7eda3aab07dd22
jungeunevepark <evecomcom98@naver.com>
2023-07-21 11:11:59 +0900

    :art: refactor: MemberInfoArgumentResolver에서 resolveArgument의 반환 값을 MemberInfoDto로 수정

--- backend/src/main/java/com/ashe/popping/global/resolver/memberinfo/MemberInfoArgumentResolver.java
+++ backend/src/main/java/com/ashe/popping/global/resolver/memberinfo/MemberInfoArgumentResolver.java
@@ -7,7 +7,6 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-import com.ashe.popping.domain.member.constant.Role;
 import com.ashe.popping.global.jwt.service.TokenManager;
 
 import io.jsonwebtoken.Claims;
@@ -21,30 +20,31 @@ public class MemberInfoArgumentResolver implements HandlerMethodArgumentResolver
 	private final TokenManager tokenManager;
 
 	/*
-	* 주어진 메소드의 파리미터가 이 Argument Resolver에서 지원하는 타입인지 검사
-	* 맞다면 true, 아니라면 false를 반환
-	* */
+	 * 주어진 메소드의 파리미터가 이 Argument Resolver에서 지원하는 타입인지 검사
+	 * 맞다면 true, 아니라면 false를 반환
+	 * */
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
 		boolean hasMemberInfoAnnotaion = parameter.hasParameterAnnotation(MemberInfo.class);
 		boolean hasMemberInfoDto = MemberInfoDto.class.isAssignableFrom(parameter.getParameterType());
 		return hasMemberInfoAnnotaion && hasMemberInfoDto;
 	}
+
 	/*
-	* 이 메소드의 반환값이 대상이 되는 메소드의 파라미터에 바인딩된다.
-	*
-	* @MemberInfo 어노테이션이 호출되었을 때 실행되는 부분
-	* */
+	 * 이 메소드의 반환값이 대상이 되는 메소드의 파라미터에 바인딩된다.
+	 *
+	 * @MemberInfo 어노테이션이 호출되었을 때 실행되는 부분
+	 * */
 	@Override
-	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+	public MemberInfoDto resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
 		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
 		// 토큰에 있던 정보를 MemberInfoDto로 반들어 반환
-		HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+		HttpServletRequest request = (HttpServletRequest)webRequest.getNativeRequest();
 		String authorizationHeader = request.getHeader("Authorization");
 		String token = authorizationHeader.split(" ")[1];
 		Claims tokenClaims = tokenManager.getTokenClaims(token);
-		Long memberId = Long.valueOf((Integer) tokenClaims.get("memberId"));
-		String role = (String) tokenClaims.get("role");
+		Long memberId = Long.valueOf((Integer)tokenClaims.get("memberId"));
+		String role = (String)tokenClaims.get("role");
 		return MemberInfoDto.of(memberId, role);
 	}
 }
