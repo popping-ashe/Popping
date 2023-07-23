3d38d56d51a17654a71e781f79dabe11be242bc3
jungeunevepark <evecomcom98@naver.com>
2023-07-23 15:57:25 +0900

    :sparkles: feat: 공유 id를 받았을 때, 받은 메세지 리스트를 보내주는 함수 추가

--- backend/src/main/java/com/ashe/popping/api/share/controller/ShareController.java
+++ backend/src/main/java/com/ashe/popping/api/share/controller/ShareController.java
@@ -1,16 +1,22 @@
 package com.ashe.popping.api.share.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ashe.popping.api.message.dto.MessageApiDto;
 import com.ashe.popping.domain.member.dto.MemberDto;
+import com.ashe.popping.domain.member.entity.Member;
 import com.ashe.popping.domain.member.service.MemberService;
 import com.ashe.popping.domain.message.dto.MessageDto;
+import com.ashe.popping.domain.message.service.MessageService;
+import com.ashe.popping.global.error.ErrorCode;
+import com.ashe.popping.global.error.exception.AuthenticationException;
 import com.ashe.popping.global.resolver.memberinfo.MemberInfo;
 import com.ashe.popping.global.resolver.memberinfo.MemberInfoDto;
 
@@ -22,12 +28,32 @@ import lombok.RequiredArgsConstructor;
 public class ShareController {
 
 	private final MemberService memberService;
+	private final MessageService messageService;
 
 	@GetMapping
 	public ResponseEntity<Long> getShareId(@MemberInfo MemberInfoDto memberInfoDto) {
 		Long memberId = memberInfoDto.getMemberId();
 		MemberDto memberDto = memberService.getMemberByMemberId(memberId);
 		return ResponseEntity.ok(memberDto.getShareId());
+	}
+
+	@GetMapping("/{shareId}")
+	public ResponseEntity<List<MessageApiDto.Response>> getReceivedMessage(@PathVariable Long shareId) {
+		Optional<Member> optionalMember = memberService.getMemberByShareId(shareId);
+		if(optionalMember.isEmpty()){
+			throw new AuthenticationException(ErrorCode.NOT_EXIST_MEMBER);
+		}
+		else{
+			Member shareMember = optionalMember.get();
+			List<MessageDto> messages = messageService.loadReceiveMessage(shareMember.getMemberId());
+			return ResponseEntity.ok(toMessageResponse(messages));
+		}
+	}
+
+	private static List<MessageApiDto.Response> toMessageResponse(List<MessageDto> messages) {
+		return messages.stream()
+			.map(MessageApiDto.Response::from)
+			.toList();
 	}
 
 
