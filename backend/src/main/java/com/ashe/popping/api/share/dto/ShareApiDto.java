5746e4e5309310e4e17b9f6fbad15361694f8483
jungeunevepark <evecomcom98@naver.com>
2023-07-24 16:55:06 +0900

    :sparkles: feat: 공유 api를 위한 ShareApiDto 생성

---
+++ backend/src/main/java/com/ashe/popping/api/share/dto/ShareApiDto.java
@@ -0,0 +1,66 @@
+package com.ashe.popping.api.share.dto;
+
+import java.time.LocalDateTime;
+
+import com.ashe.popping.domain.message.dto.MessageDto;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.Builder;
+import lombok.Getter;
+
+public class ShareApiDto {
+	@Getter
+	@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+	public static class Request {
+		private String content;
+		private Long sender;
+		private Long receiver;
+		private String nickname;
+		private Long retentionTime;
+
+		@Builder
+		public Request(String content, Long sender, Long receiver, String nickname, Long retentionTime) {
+			this.content = content;
+			this.sender = sender;
+			this.receiver = receiver;
+			this.nickname = nickname;
+			this.retentionTime = retentionTime;
+		}
+	}
+
+	@Getter
+	@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+	public static class Response {
+		private Long messageId;
+		private String state;
+		private Long receiver;
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+		private LocalDateTime createTime;
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+		private LocalDateTime expirationTime;
+
+		@Builder
+		public Response(Long messageId, String content, String state, Long receiver, String nickname,
+			LocalDateTime createTime,
+			LocalDateTime expirationTime) {
+			this.messageId = messageId;
+			this.state = state;
+			this.receiver = receiver;
+			this.createTime = createTime;
+			this.expirationTime = expirationTime;
+		}
+
+		public static com.ashe.popping.api.share.dto.ShareApiDto.Response from(MessageDto messageDto) {
+			return com.ashe.popping.api.share.dto.ShareApiDto.Response.builder()
+				.messageId(messageDto.getMessageId())
+				.state(messageDto.getState().getMessage())
+				.receiver(messageDto.getReceiver())
+				.createTime(messageDto.getCreateTime())
+				.expirationTime(messageDto.getExpirationTime())
+				.build();
+		}
+
+	}
+}
