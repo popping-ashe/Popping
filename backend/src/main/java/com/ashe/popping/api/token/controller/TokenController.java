48e62fecfe7f671cfc6e3a497c7eecf02189944d
jungeunevepark <evecomcom98@naver.com>
2023-07-24 14:19:41 +0900

    :sparkles: feat: @GetMapping으로 수정 및 refresh token type 체크 로직 추가

--- backend/src/main/java/com/ashe/popping/api/token/controller/TokenController.java
+++ backend/src/main/java/com/ashe/popping/api/token/controller/TokenController.java
@@ -1,13 +1,18 @@
 package com.ashe.popping.api.token.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ashe.popping.api.token.service.TokenService;
+import com.ashe.popping.global.error.ErrorCode;
+import com.ashe.popping.global.error.exception.AuthenticationException;
+import com.ashe.popping.global.jwt.contant.TokenType;
 import com.ashe.popping.global.jwt.dto.JwtTokenDto;
+import com.ashe.popping.global.jwt.service.TokenManager;
 import com.ashe.popping.global.util.AuthorizationHeaderUtils;
 
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
@@ -15,12 +20,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TokenController {
 	private final TokenService tokenService;
+	private final TokenManager tokenManager;
 
-	@PostMapping("/access-token/issue")
+	@GetMapping("/access-token/issue")
 	public ResponseEntity<JwtTokenDto> createToken(HttpServletRequest httpServletRequest) {
 		String authorizationHeader = httpServletRequest.getHeader("Authorization");
 		AuthorizationHeaderUtils.validateAuthorization(authorizationHeader);
 		String refreshToken = authorizationHeader.split(" ")[1];
+		Claims tokenClaims = tokenManager.getTokenClaims(refreshToken);
+		String tokenType = tokenClaims.getSubject();
+		if (!TokenType.isRefreshToken(tokenType)) {
+			throw new AuthenticationException(ErrorCode.NOT_REFRESH_TOKEN_TYPE);
+		}
 		JwtTokenDto jwtTokenDto = tokenService.createTokenByRefreshToken(refreshToken);
 		return ResponseEntity.ok(jwtTokenDto);
 
