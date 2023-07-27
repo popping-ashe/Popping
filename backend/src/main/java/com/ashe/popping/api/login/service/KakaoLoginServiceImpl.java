0bbe24b97b3b434b01e2229823b2eb2465ea2bf7
jungeunevepark <evecomcom98@naver.com>
2023-07-27 09:52:03 +0900

    :sparkles: feat: 웰컴 메세지 보내주는 로직 생성

--- backend/src/main/java/com/ashe/popping/api/login/service/KakaoLoginServiceImpl.java
+++ backend/src/main/java/com/ashe/popping/api/login/service/KakaoLoginServiceImpl.java
@@ -4,14 +4,18 @@ import java.security.SecureRandom;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.ashe.popping.api.login.client.KakaoTokenClient;
 import com.ashe.popping.api.login.dto.KakaoTokenDto;
+import com.ashe.popping.api.message.dto.MessageApiDto;
 import com.ashe.popping.domain.member.constant.Role;
 import com.ashe.popping.domain.member.dto.MemberDto;
 import com.ashe.popping.domain.member.entity.Member;
 import com.ashe.popping.domain.member.service.MemberService;
+import com.ashe.popping.domain.message.dto.MessageDto;
+import com.ashe.popping.domain.message.service.MessageService;
 import com.ashe.popping.external.oauth.kakao.dto.KakaoMemberInfoResponseDto;
 import com.ashe.popping.external.oauth.kakao.service.KakaoLoginApiService;
 import com.ashe.popping.global.jwt.contant.GrantType;
@@ -29,6 +33,7 @@ public class KakaoLoginServiceImpl implements KakaoLoginService {
 	private final KakaoLoginApiService kakaoLoginApiService;
 	private final TokenManager tokenManager;
 	private final MemberService memberService;
+	private final MessageService messageService;
 
 	@Value("${kakao.client.id}")
 	private String clientId;
@@ -50,6 +55,12 @@ public class KakaoLoginServiceImpl implements KakaoLoginService {
 		if (optionalMember.isEmpty()) {
 			// 공유 url에 필요한 난수 생성 및 회원 생성
 			MemberDto oauthMember = makeShareId(memberInfo);
+			// 웰컴 메세지 보내기
+			MessageDto wellComeMessage = makeWellComeMessage();
+			MessageDto messageDto = messageService.saveMessage(wellComeMessage, oauthMember);
+			ResponseEntity.ok(MessageApiDto.Response.from(messageDto));
+
+
 			// 토큰 생성
 			jwtTokenDto = tokenManager.createJwtTokenDto(oauthMember.getMemberId(), oauthMember.getRole());
 		}
@@ -79,5 +90,13 @@ public class KakaoLoginServiceImpl implements KakaoLoginService {
 			}
 		}
 		return oauthMember;
+	}
+
+	MessageDto makeWellComeMessage(){
+		String content = "환영합니다!\nShare 버튼을 눌러 내 페이지를 공유하세요!\n제한 시간이 지나면 버블은 사라집니다 얼른 읽어주세요\n버블 만들기를 눌러 다른 사람 페이지에 버블을 남겨보세요!";
+		String nickname = "poppping 개발자";
+		Long retentionTime = 24L;
+		MessageApiDto.Request request = MessageApiDto.Request.from(content, nickname, retentionTime);
+		return MessageDto.from(request);
 	}
 }
