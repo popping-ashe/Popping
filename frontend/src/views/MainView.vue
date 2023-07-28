98db6df572c59f90815df9d307ffdd100fa81164
YJS96 <wnt9864@hotmail.com>
2023-07-28 16:08:23 +0900

    :sparkles: feat: 메인 디자인 적용

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -3,24 +3,20 @@
     <MessageDetail :bubbledetail-props="bubbleDetail" class="message-detail" v-if="showReceivedDetail"/>
     <MakeBubble v-if="showMakeWindow"/>
     <div class="upper-bar">
-      <div class="share-ellipse">
-        <div v-if="pageid == shareid" class="share-emoji" @click="shareCopy()">
-          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path d="M400 255.4V240 208c0-8.8-7.2-16-16-16H352 336 289.5c-50.9 0-93.9 33.5-108.3 79.6c-3.3-9.4-5.2-19.8-5.2-31.6c0-61.9 50.1-112 112-112h48 16 32c8.8 0 16-7.2 16-16V80 64.6L506 160 400 255.4zM336 240h16v48c0 17.7 14.3 32 32 32h3.7c7.9 0 15.5-2.9 21.4-8.2l139-125.1c7.6-6.8 11.9-16.5 11.9-26.7s-4.3-19.9-11.9-26.7L409.9 8.9C403.5 3.2 395.3 0 386.7 0C367.5 0 352 15.5 352 34.7V80H336 304 288c-88.4 0-160 71.6-160 160c0 60.4 34.6 99.1 63.9 120.9c5.9 4.4 11.5 8.1 16.7 11.2c4.4 2.7 8.5 4.9 11.9 6.6c3.4 1.7 6.2 3 8.2 3.9c2.2 1 4.6 1.4 7.1 1.4h2.5c9.8 0 17.8-8 17.8-17.8c0-7.8-5.3-14.7-11.6-19.5l0 0c-.4-.3-.7-.5-1.1-.8c-1.7-1.1-3.4-2.5-5-4.1c-.8-.8-1.7-1.6-2.5-2.6s-1.6-1.9-2.4-2.9c-1.8-2.5-3.5-5.3-5-8.5c-2.6-6-4.3-13.3-4.3-22.4c0-36.1 29.3-65.5 65.5-65.5H304h32zM72 32C32.2 32 0 64.2 0 104V440c0 39.8 32.2 72 72 72H408c39.8 0 72-32.2 72-72V376c0-13.3-10.7-24-24-24s-24 10.7-24 24v64c0 13.3-10.7 24-24 24H72c-13.3 0-24-10.7-24-24V104c0-13.3 10.7-24 24-24h64c13.3 0 24-10.7 24-24s-10.7-24-24-24H72z"/></svg>
-          <!-- <img src="../assets/홈.png" alt="홈" class="icons" > -->
+      <div class="new-button font-eng" style="margin-left: 6%">
+        <div v-if="pageid == shareid" @click="shareCopy()">
+          Share
         </div>
-        <div v-else class="tohome-emoji" @click="toHome()">
-          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c.2 35.5-28.5 64.3-64 64.3H128.1c-35.3 0-64-28.7-64-64V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24zM352 224a64 64 0 1 0 -128 0 64 64 0 1 0 128 0zm-96 96c-44.2 0-80 35.8-80 80c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16c0-44.2-35.8-80-80-80H256z"/></svg>
+        <div v-else @click="toHome()">
+          Home
         </div>
       </div>
-      <div class="username font-stardust" @click="generateRandomSizes()">
+      <div class="username font-kor" @click="generateRandomSizes()">
         <!-- 본인페이지 여부에 따라 표시 -->
-        {{ this.nickname }}'s<br>
-        BUBBLE
+        {{ this.nickname }}'s BUBBLE
       </div>
-      <div class="mypage-ellipse" @click="$router.push('/mypage')">
-        <div class="mypage-emoji">
-          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z"/></svg>
-        </div>
+      <div class="new-button font-eng" style="margin-right: 6%" @click="$router.push('/mypage')">
+        My
       </div>
     </div>
 
@@ -33,12 +29,12 @@
       <img class="bubble2" src="../assets/bubble.png" alt=""> -->
     </div>
 
-    <div class="under-bar">
       <!-- 로그인 상태에 따라 동적으로 메세지 보내기 버튼 활성화/비활성화 -->
-      <div class="bubble-make-btn font-stardust" v-if="pageid != shareid" @click="openMake()">
-        <div>버블 만들기</div>
-      </div>
+    <div class="bubble-make-btn font-kor" v-if="pageid != shareid" @click="openMake()">
+      <img class="bubble-make-image"  src="../assets/makebubblebtn.png" alt="">
+      <div class="bubble-make-text">버블 만들기</div>
     </div>
+
   </div>
 </template>
 
@@ -239,6 +235,12 @@ export default {
 </script>
 
 <style scoped>
+@font-face {
+    font-family: 'hydrophilia';
+    src: url('../assets/fonts/hydrophilia-iced-regular.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+}
 
 #index {
   display:none;
@@ -268,81 +270,45 @@ export default {
 
   }
 
-.share-ellipse {
+.new-button {
+  width: 65px;
+  height: 29px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
   box-sizing: border-box;
-  width: 40px;
-  height: 40px;
-  margin-left: 6%;
-  background: #FFFFFF;
-  border: 1px solid #505050;
-  border-radius: 50%;
-  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.25);
-  transition: all ease 0.4s;
+  border-style : solid;
+  border-color : #808384;
+  border-color : rgba(128, 131, 132, 1);
+  border-width : 1px;
+  border-radius : 16px;
+  -moz-border-radius : 16px;
+  -webkit-border-radius : 16px;
+  box-shadow: inset 0px -3px 3px #D8D8D8;
+  padding :2px 10px 4px;
+  text-decoration: none;
+  font-family: 'hydrophilia';
+  color:black;
+  font-size: 14px;
+  font-weight: 600;
   }
-
-  .share-ellipse:hover {
-    filter: brightness(70%);
-    transition: all ease 0.4s;
-  }
-
-.mypage-ellipse {
-  box-sizing: border-box;
-
-  width: 40px;
-  height: 40px;
-  margin-right: 6%;
-  background: #FFFFFF;
-  border: 1px solid #505050;
-  border-radius: 50%;
-  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.25);
-  transition: all ease 0.4s;
-  }
-
-.mypage-ellipse:hover {
-  filter: brightness(70%);
-  transition: all ease 0.4s;
-  }
-
-.share-emoji {
-  position: relative;
-  width: 32px;
-  height: 32px;
-  left: 12px;
-  top: 4px;
-}
-
-.tohome-emoji {
-  position: relative;
-  width: 32px;
-  height: 32px;
-  left: 10.5px;
-  top: 4.2px;
-}
-
-.mypage-emoji {
-  position: relative;
-  width: 32px;
-  height: 32px;
-  left: 12px;
-  top: 4px;
-}
 
 .username {
   width: 176px;
   height: 48px;
   justify-content: center;
+  margin-top: 2px;
 
-  font-family: 'Inter';
   font-style: normal;
   font-weight: 400;
-  font-size: 20px;
+  font-size: 14.5px;
   line-height: 21px;
   /* or 105% */
   display: flex;
   align-items: center;
   text-align: center;
   letter-spacing: -0.32px;
-  font-family: '글씨체';
   color: #000000;
 
   text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.25);
@@ -351,17 +317,13 @@ export default {
 .bubble-area {
   position: absolute;
   width: 95%;
-  height: 69%;
-  top: 16.4%;
+  height: 70.5%;
+  top: 13%;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
   overflow: scroll;
   }
-
-::-webkit-scrollbar {
-  display: none;
-}
 
 .bubble {
   position: relative;
@@ -382,37 +344,33 @@ export default {
   }
 }
 
-.under-bar {
-  position: absolute;
-  width: 100%;
-  height: 9.3%;
-  top: 86%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
 .bubble-make-btn {
-  box-sizing: border-box;
-
-  position: absolute;
-  width: 268px;
-  height: 50px;
-  border: 1px solid #000000;
-  border-radius: 50px;
+  position: relative;
+  top: 79%;
+  width: 100%;
+  height: 10%;
   display: flex;
   justify-content: center;
   align-items: center;
-  filter: drop-shadow(1px 1px 3px rgba(0, 0, 0, 0.25));
-  font-family: '글씨체';
-  transition: all ease 0.4s;
   }
 
-.bubble-make-btn:hover {
-  transition: all ease 0.4s;
+.bubble-make-image {
+  width: 78%;
+  height: 85%;
 }
 
-.font-stardust{
-  font-family: 'Stardust'
+.bubble-make-text {
+  position: absolute;
+  margin-bottom: 2.5%;
 }
+
+.font-kor{
+  font-family: 'Galmuri9';
+  font-weight: 200;
+}
+
+.font-eng{
+  font-family: 'hydrophilia'
+}
+
 </style>
