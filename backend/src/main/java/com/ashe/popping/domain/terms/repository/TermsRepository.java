fc9a08a8be47ebf42c6b858ab8257aa1b592f73b
Mina Park <mineezm9@gmail.com>
2023-08-06 03:02:38 +0900

    ✨ feat: 약관id로 약관 정보 리턴하는 쿼리 메서드 구현

--- backend/src/main/java/com/ashe/popping/domain/terms/repository/TermsRepository.java
+++ backend/src/main/java/com/ashe/popping/domain/terms/repository/TermsRepository.java
@@ -8,4 +8,6 @@ import com.ashe.popping.domain.terms.entity.Terms;
 
 public interface TermsRepository extends JpaRepository<Terms, Long> {
 	List<Terms> findAll();
+
+	Terms findByTermsId(Long termsId);
 }
