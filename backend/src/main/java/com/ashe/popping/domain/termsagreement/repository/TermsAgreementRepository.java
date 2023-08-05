374fbe354e40dd25ba17426daeed43cfb57afd09
Mina Park <mineezm9@gmail.com>
2023-08-06 05:02:58 +0900

    ✨ feat: TermsAgreement Repository 구현

--- backend/src/main/java/com/ashe/popping/domain/termsagreement/repository/TermsAgreementRepository.java
+++ backend/src/main/java/com/ashe/popping/domain/termsagreement/repository/TermsAgreementRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ashe.popping.domain.termsagreement.dto.TermsAgreementDto;
 import com.ashe.popping.domain.termsagreement.entity.TermsAgreement;
 
 public interface TermsAgreementRepository extends JpaRepository<TermsAgreement, Long> {
