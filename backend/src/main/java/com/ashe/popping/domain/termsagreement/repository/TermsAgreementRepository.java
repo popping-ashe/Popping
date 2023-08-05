278cc3a99ebebc159b0d66c0f65f5e230228504e
Mina Park <mineezm9@gmail.com>
2023-08-06 04:37:32 +0900

    ✨ feat: 멤버 id로 멤버의 약관 동의 이력을 가져오는 쿼리 메서드 구현

---
+++ backend/src/main/java/com/ashe/popping/domain/termsagreement/repository/TermsAgreementRepository.java
@@ -0,0 +1,11 @@
+package com.ashe.popping.domain.termsagreement.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ashe.popping.domain.termsagreement.entity.TermsAgreement;
+
+public interface TermsAgreementRepository extends JpaRepository<TermsAgreement, Long> {
+	List<TermsAgreement> findByMemberId(Long memberId);
+}
