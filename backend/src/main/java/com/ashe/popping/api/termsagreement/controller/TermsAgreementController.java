b3cbb57bbe361a6c3b8fdb57af8fe4c11d2ab7e6
Mina Park <mineezm9@gmail.com>
2023-08-06 06:37:04 +0900

    ✨ feat: 약관 동의 데이터를 생성하는 POST API `/terms-agreement` 구현

---
+++ backend/src/main/java/com/ashe/popping/api/termsagreement/controller/TermsAgreementController.java
@@ -0,0 +1,33 @@
+package com.ashe.popping.api.termsagreement.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ashe.popping.api.termsagreement.dto.TermsAgreementApiDto;
+import com.ashe.popping.domain.terms.service.TermsService;
+import com.ashe.popping.domain.termsagreement.dto.TermsAgreementDto;
+import com.ashe.popping.domain.termsagreement.service.TermsAgreementService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/terms-agreement")
+@RequiredArgsConstructor
+public class TermsAgreementController {
+	private final TermsAgreementService termsAgreementService;
+	private final TermsService termsService;
+
+	@PostMapping
+	public ResponseEntity<TermsAgreementApiDto.Response> createTermsAgreement(@RequestBody TermsAgreementApiDto.Request request) {
+		TermsAgreementDto termsAgreementDto = termsAgreementService.createTermsAgreement(TermsAgreementDto.from(request));
+		return ResponseEntity.ok(TermsAgreementApiDto.Response.of(termsService.getTerms(request.getTermsId()), termsAgreementDto));
+	}
+}
