1db48f0afeb619fa9b45b3463938c8d92d246a52
Mina Park <mineezm9@gmail.com>
2023-08-06 04:08:43 +0900

    ✨ feat: 약관을 생성하는 POST API `/terms` 구현

---
+++ backend/src/main/java/com/ashe/popping/api/terms/controller/TermsController.java
@@ -0,0 +1,30 @@
+package com.ashe.popping.api.terms.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ashe.popping.api.terms.dto.TermsApiDto;
+import com.ashe.popping.domain.terms.dto.TermsDto;
+import com.ashe.popping.domain.terms.service.TermsService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/terms")
+@RequiredArgsConstructor
+public class TermsController {
+	private final TermsService termsService;
+
+	@PostMapping
+	public ResponseEntity<TermsApiDto.Response> createTerms(@RequestBody TermsApiDto.Request request) {
+		TermsDto terms = termsService.createTerms(TermsDto.from(request));
+		return ResponseEntity.ok(TermsApiDto.Response.from(terms));
+	}
+}
