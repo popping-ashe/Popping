1d749f7cf13789c0fc750dae54bc7e71761ba02d
fluffymn <mineezm9@gmail.com>
2023-08-08 14:02:54 +0900

    🎨 refactor: Terms 쿼리 메서드에서 Optional로 리턴하여 에러처리 추가

--- backend/src/main/java/com/ashe/popping/domain/terms/repository/TermsRepository.java
+++ backend/src/main/java/com/ashe/popping/domain/terms/repository/TermsRepository.java
@@ -1,6 +1,7 @@
 package com.ashe.popping.domain.terms.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,7 +10,7 @@ import com.ashe.popping.domain.terms.entity.Terms;
 public interface TermsRepository extends JpaRepository<Terms, Long> {
 	List<Terms> findAll();
 
-	Terms findByTermsId(Long termsId);
+	Optional<Terms> findByTermsId(Long termsId);
 
 	Long deleteByTermsId(Long termsId);
 }
