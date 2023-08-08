42b3518eb563dcf779a15a5c7024ecb84f6029f2
fluffymn <mineezm9@gmail.com>
2023-08-08 09:32:28 +0900

    ✨ feat: 회원 가입 할때 모든 약관의 동의 이력이 PENDING 상태로 약관 동의 테이블에 들어가도록 구현

--- backend/src/main/java/com/ashe/popping/api/login/service/KakaoLoginServiceImpl.java
+++ backend/src/main/java/com/ashe/popping/api/login/service/KakaoLoginServiceImpl.java
@@ -1,10 +1,10 @@
 package com.ashe.popping.api.login.service;
 
 import java.security.SecureRandom;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.ashe.popping.api.login.client.KakaoTokenClient;
@@ -16,6 +16,11 @@ import com.ashe.popping.domain.member.entity.Member;
 import com.ashe.popping.domain.member.service.MemberService;
 import com.ashe.popping.domain.message.dto.MessageDto;
 import com.ashe.popping.domain.message.service.MessageService;
+import com.ashe.popping.domain.terms.dto.TermsDto;
+import com.ashe.popping.domain.terms.service.TermsService;
+import com.ashe.popping.domain.termsagreement.dto.TermsAgreementDto;
+import com.ashe.popping.domain.termsagreement.dto.TermsAgreementState;
+import com.ashe.popping.domain.termsagreement.service.TermsAgreementService;
 import com.ashe.popping.external.oauth.kakao.dto.KakaoMemberInfoResponseDto;
 import com.ashe.popping.external.oauth.kakao.service.KakaoLoginApiService;
 import com.ashe.popping.global.jwt.contant.GrantType;
@@ -34,6 +39,8 @@ public class KakaoLoginServiceImpl implements KakaoLoginService {
 	private final TokenManager tokenManager;
 	private final MemberService memberService;
 	private final MessageService messageService;
+	private final TermsService termsService;
+	private final TermsAgreementService termsAgreementService;
 
 	@Value("${kakao.client.id}")
 	private String clientId;
@@ -58,6 +65,13 @@ public class KakaoLoginServiceImpl implements KakaoLoginService {
 			// 웰컴 메세지 보내기
 			MessageDto wellComeMessage = makeWellComeMessage();
 			messageService.saveMessage(wellComeMessage, oauthMember);
+
+			List<TermsDto> terms = termsService.getAllTerms();
+
+			terms.forEach(t -> termsAgreementService.createTermsAgreement(
+				TermsAgreementDto.of(oauthMember.getMemberId(), t.getTermsId(),
+					TermsAgreementState.PENDING)));
+
 			// 토큰 생성
 			jwtTokenDto = tokenManager.createJwtTokenDto(oauthMember.getMemberId(), oauthMember.getRole());
 		}
@@ -70,26 +84,26 @@ public class KakaoLoginServiceImpl implements KakaoLoginService {
 
 		return jwtTokenDto;
 
-
 	}
-	MemberDto makeShareId(KakaoMemberInfoResponseDto memberInfo){
+
+	MemberDto makeShareId(KakaoMemberInfoResponseDto memberInfo) {
 		SecureRandom random = new SecureRandom();
 		MemberDto oauthMember;
 
-		while(true){
+		while (true) {
 			Long shareId = random.nextLong(1000000000L, 10000000000L);
 			oauthMember = memberInfo.toMemberDto(Role.USER, shareId);
-			try{
+			try {
 				oauthMember = memberService.createMember(oauthMember);
 				break;
-			}catch(Exception e){
+			} catch (Exception e) {
 				log.info("중복 공유 아이디 생성", e);
 			}
 		}
 		return oauthMember;
 	}
 
-	MessageDto makeWellComeMessage(){
+	MessageDto makeWellComeMessage() {
 		String content = "\uD83D\uDC99환영합니다\uD83D\uDC99\n"
 			+ "제한 시간이 지나면 버블은 사라지니 얼른 읽어주세요⏰\n"
 			+ "친구의 링크를 공유받아 버블을 남겨보세요\uD83D\uDD17";
