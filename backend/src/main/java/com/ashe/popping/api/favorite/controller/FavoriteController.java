81079d778aa4a3a7bf991eab254361fef8584e23
김지은 <jxixeun@gmail.com>
2023-08-09 00:07:15 +0900

    :sparkles: feat: 즐겨찾기 조회, 해제 API 구현

--- backend/src/main/java/com/ashe/popping/api/favorite/controller/FavoriteController.java
+++ backend/src/main/java/com/ashe/popping/api/favorite/controller/FavoriteController.java
@@ -1,6 +1,8 @@
 package com.ashe.popping.api.favorite.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ashe.popping.api.favorite.dto.FavoriteApiDto;
 import com.ashe.popping.domain.favorite.dto.FavoriteDto;
+import com.ashe.popping.domain.favorite.dto.FavoriteListDto;
 import com.ashe.popping.domain.favorite.service.FavoriteService;
 import com.ashe.popping.global.resolver.memberinfo.MemberInfo;
 import com.ashe.popping.global.resolver.memberinfo.MemberInfoDto;
@@ -27,4 +30,18 @@ public class FavoriteController {
 		FavoriteDto favoriteDto = favoriteService.saveFavorite(FavoriteDto.from(request, memberId));
 		return ResponseEntity.ok(FavoriteApiDto.Response.from(favoriteDto));
 	}
+	@GetMapping
+	public ResponseEntity<FavoriteApiDto.ListResponse> getFavorites(@MemberInfo MemberInfoDto memberInfoDto) {
+		Long memberId = memberInfoDto.getMemberId();
+		FavoriteListDto favoriteListDto = favoriteService.loadFavorites(memberId);
+		return ResponseEntity.ok(FavoriteApiDto.ListResponse.from(favoriteListDto));
+	}
+
+	@DeleteMapping
+	public ResponseEntity<?> unFavorite(@MemberInfo MemberInfoDto memberInfoDto, @RequestBody FavoriteApiDto.Request request){
+		Long memberId = memberInfoDto.getMemberId();
+		favoriteService.removeFavorite(FavoriteDto.from(request, memberId));
+		return ResponseEntity.ok(null);
+	}
+
 }
