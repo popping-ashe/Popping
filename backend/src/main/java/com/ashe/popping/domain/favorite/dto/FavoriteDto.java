69cf1a046fbc4fe4e4b60910b4fe10b8d858a990
jxixeun <jxixeun@gmail.com>
2023-08-08 17:25:44 +0900

    :sparkles: feat: FavoriteDto 생성 및 from 메서드 구현

---
+++ backend/src/main/java/com/ashe/popping/domain/favorite/dto/FavoriteDto.java
@@ -0,0 +1,25 @@
+package com.ashe.popping.domain.favorite.dto;
+
+import com.ashe.popping.domain.favorite.entity.Favorite;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class FavoriteDto {
+	private Long memberId;
+	private Long favoriteId;
+
+	@Builder
+	public FavoriteDto(Long memberId, Long favoriteId) {
+		this.memberId = memberId;
+		this.favoriteId = favoriteId;
+	}
+
+	static public FavoriteDto from(Favorite favorite) {
+		return FavoriteDto.builder()
+			.memberId(favorite.getMemberId())
+			.favoriteId(favorite.getFavoriteId())
+			.build();
+	}
+}
