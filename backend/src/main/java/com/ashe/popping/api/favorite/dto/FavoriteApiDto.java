1aadc976a027f71fed424ebc024d7914b3d85b2e
김지은 <jxixeun@gmail.com>
2023-08-08 23:57:56 +0900

    :sparkles: feat: ListResponse from 메서드 작성 및 필드명 수정, 기본생성자 어노테이션 추가

--- backend/src/main/java/com/ashe/popping/api/favorite/dto/FavoriteApiDto.java
+++ backend/src/main/java/com/ashe/popping/api/favorite/dto/FavoriteApiDto.java
@@ -3,14 +3,18 @@ package com.ashe.popping.api.favorite.dto;
 import java.util.List;
 
 import com.ashe.popping.domain.favorite.dto.FavoriteDto;
+import com.ashe.popping.domain.favorite.dto.FavoriteListDto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class FavoriteApiDto {
 	@Getter
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
 	@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 	public static class Request {
 		private Long favoriteId;
@@ -45,12 +49,19 @@ public class FavoriteApiDto {
 	@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 	public static class ListResponse {
 		private Long memberId;
-		private List<Long> favoriteId;
+		private List<Long> favorites;
 
 		@Builder
-		public ListResponse(Long memberId, List<Long> favoriteId) {
+		public ListResponse(Long memberId, List<Long> favorites) {
 			this.memberId = memberId;
-			this.favoriteId = favoriteId;
+			this.favorites = favorites;
+		}
+
+		public static ListResponse from(FavoriteListDto favoriteListDto){
+			return ListResponse.builder()
+				.memberId(favoriteListDto.getMemberId())
+				.favorites(favoriteListDto.getFavorites())
+				.build();
 		}
 	}
 }
