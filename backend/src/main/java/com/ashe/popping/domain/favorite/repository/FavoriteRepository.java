6e0de9048c595334ac5c59c2af4b85a875b3098a
jxixeun <jxixeun@gmail.com>
2023-08-08 17:24:26 +0900

    :sparkles: feat: Favorite Repository 인터페이스 생성

---
+++ backend/src/main/java/com/ashe/popping/domain/favorite/repository/FavoriteRepository.java
@@ -0,0 +1,13 @@
+package com.ashe.popping.domain.favorite.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ashe.popping.domain.favorite.entity.Favorite;
+import com.ashe.popping.domain.favorite.entity.FavoriteMapping;
+import com.ashe.popping.domain.favorite.entity.FavoritePK;
+
+public interface FavoriteRepository extends JpaRepository<Favorite, FavoritePK> {
+	List<FavoriteMapping> findByMemberId(Long memberId);
+}
