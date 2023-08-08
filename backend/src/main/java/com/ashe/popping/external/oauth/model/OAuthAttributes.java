b14227904525bf80d24bee2ccb40b12f418ad1cb
jungeunevepark <evecomcom98@naver.com>
2023-08-08 17:43:03 +0900

    ✨ feat: OAuthAttributes 정적 팩토리 메서드 생성

--- backend/src/main/java/com/ashe/popping/external/oauth/model/OAuthAttributes.java
+++ backend/src/main/java/com/ashe/popping/external/oauth/model/OAuthAttributes.java
@@ -1,6 +1,8 @@
 package com.ashe.popping.external.oauth.model;
 
 import com.ashe.popping.domain.member.constant.MemberType;
+import com.ashe.popping.domain.member.constant.Role;
+import com.ashe.popping.domain.member.entity.Member;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -11,4 +13,22 @@ public class OAuthAttributes {
 	private String id;
 	private String nickname;
 	private MemberType memberType;
+
+
+	public static OAuthAttributes of(String id, String nickname, MemberType memberType){
+		return OAuthAttributes.builder()
+			.id(id)
+			.nickname(nickname)
+			.memberType(memberType)
+			.build();
+	}
+
+	public Member toMemberEntity(MemberType memberType, Role role){
+		return Member.builder()
+			.memberType(memberType)
+			.socialLoginId(id)
+			.nickname(nickname)
+			.role(role)
+			.build();
+	}
 }
