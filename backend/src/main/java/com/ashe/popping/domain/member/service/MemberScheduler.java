9a6a181e4df25021138e6744b432b636e4aefb25
jxixeun <jxixeun@gmail.com>
2023-08-10 14:25:20 +0900

    :sparkles: feat: 스케줄러에서 멤버를 삭제할 때 Favorite 테이블에서 관련된 데이터를 모두 삭제하기 구현

--- backend/src/main/java/com/ashe/popping/domain/member/service/MemberScheduler.java
+++ backend/src/main/java/com/ashe/popping/domain/member/service/MemberScheduler.java
@@ -1,10 +1,13 @@
 package com.ashe.popping.domain.member.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.ashe.popping.domain.favorite.service.FavoriteService;
+import com.ashe.popping.domain.member.entity.Member;
 import com.ashe.popping.domain.member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -16,10 +19,17 @@ import lombok.extern.slf4j.Slf4j;
 public class MemberScheduler {
 
 	private final MemberRepository memberRepository;
+	private final FavoriteService favoriteService;
 
 	@Scheduled(cron = "0 0 5 * * *", zone = "Asia/Seoul")
 	public void removeMember() {
-		int countDeletedMember = memberRepository.deleteByWithdrawalDateBefore(LocalDateTime.now());
+		LocalDateTime now = LocalDateTime.now();
+		List<Member> withdrawalMember = memberRepository.findByWithdrawalDateBefore(now);
+		withdrawalMember.stream()
+			.map(Member::getMemberId)
+			.toList()
+			.forEach(favoriteService::removeWithdrawalMember);
+		int countDeletedMember = memberRepository.deleteByWithdrawalDateBefore(now);
 		log.info("Number of members deleted after 30 days : {}", countDeletedMember);
 	}
 
