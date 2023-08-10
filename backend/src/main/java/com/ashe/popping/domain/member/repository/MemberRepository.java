ce52fddfdbc3aec82468eb829eba7131e0403c4c
jxixeun <jxixeun@gmail.com>
2023-08-10 14:02:49 +0900

    :sparkles: feat: 회원 정보 보관 기간이 지난 회원을 탈퇴시키는 메서드 작성

--- backend/src/main/java/com/ashe/popping/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/ashe/popping/domain/member/repository/MemberRepository.java
@@ -1,11 +1,15 @@
 package com.ashe.popping.domain.member.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import com.ashe.popping.domain.member.entity.Member;
+
+import jakarta.transaction.Transactional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	Optional<Member> findByMemberId(Long memberId);
@@ -17,4 +21,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	Long deleteByMemberId(Long memberId);
 
 	List<Member> findAll();
+
+	@Modifying(clearAutomatically = true)
+	@Transactional
+	int deleteByWithdrawalDateBefore(LocalDateTime now);
 }
