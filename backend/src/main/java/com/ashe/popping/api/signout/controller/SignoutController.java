fe4c97428d01e1b763d16d60db52bb8e34f2f4dd
양정훈 <yjhoon3658@naver.com>
2023-08-10 10:39:35 +0900

    :fire: remove: 사용하지 않는 import 삭제

--- backend/src/main/java/com/ashe/popping/api/signout/controller/SignoutController.java
+++ backend/src/main/java/com/ashe/popping/api/signout/controller/SignoutController.java
@@ -1,7 +1,6 @@
 package com.ashe.popping.api.signout.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
