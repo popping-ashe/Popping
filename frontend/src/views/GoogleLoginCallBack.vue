905e9f01cb5670190fbc59771d198b3d9a948e60
jungeunevepark <evecomcom98@naver.com>
2023-08-11 09:46:27 +0900

    :sparkles: feat: 구글 로그인 콜백 vue 생성

---
+++ frontend/src/views/GoogleLoginCallBack.vue
@@ -0,0 +1,30 @@
+<template>
+  <div></div>
+</template>
+<script>
+import { mapState, mapActions } from "vuex";
+
+const userStore = "userStore";
+
+export default {
+  name: "LoginCallBack",
+  components: {},
+  data() {
+    return {
+      message: "",
+    };
+  },
+  computed: {
+    ...mapState(userStore, ["isLogin", "isLoginError", "userInfo"]),
+  },
+  async created() {
+    let code = this.$route.query.code;
+    this.google(code);
+  },
+  methods: {
+    ...mapActions(userStore, ["google"]),
+  },
+};
+</script>
+
+<style scoped></style>
